67202889ba46116b543b662585e481ad878195c9
Jeeves Assistant <jeevesassistant00@gmail.com>
2026-07-26 10:01:20 -0500

    carry: Discord auto-thread channel controls

--- tests/tools/test_local_env_blocklist.py
+++ tests/tools/test_local_env_blocklist.py
@@ -428,6 +428,7 @@ class TestBlocklistCoverage:
             "DISCORD_HOME_CHANNEL_NAME",
             "DISCORD_REQUIRE_MENTION",
             "DISCORD_FREE_RESPONSE_CHANNELS",
+            "DISCORD_AUTO_THREAD_CHANNELS",
             "DISCORD_AUTO_THREAD",
             "SLACK_HOME_CHANNEL",
             "SLACK_HOME_CHANNEL_NAME",
